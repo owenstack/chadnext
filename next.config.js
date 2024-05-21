607b11a0f4cfb49822e6c2057fe88af0956c423b
owenEEfobi <jenukohaefobi@gmail.com>
2024-05-21 08:26:10 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const { withContentlayer } = require("next-contentlayer");
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV !== "production",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/dashboard",
+        destination: "/dashboard/projects",
+        permanent: false,
+      },
+    ];
+  },
+  trailingSlash: true,
+};
+
+module.exports = withPWA(withContentlayer(nextConfig));
